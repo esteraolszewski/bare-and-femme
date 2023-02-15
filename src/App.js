eafdb616f2f920eb6f3939ba454f360cbf711a6c
esteraolszewski <estera.o@live.ca>
2023-02-15 15:19:34 -0500

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
-  return <Home/>;
+  return <Cart/>;
 }
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+    ), 
+    url("https://media.istockphoto.com/id/1171746484/photo/still-life-home-comfort-in-the-living-room.jpg?s=612x612&w=0&k=20&c=H_f8xWqHVX8q8MOz_N3XvQPKi0kSif_Mz45inigqleE=") center no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: lightpink;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+`
+
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`
+
+const Login = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="username"/>
+                    <Input placeholder="password"/>
+                    <Button>LOGIN</Button>
+                    <Link>FORGOT PASSWORD</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Login;
